5f95c727b16be4cc5056113ea18dbf751e5996cd
Anubhab Mukherjee <78078306+Anubhab-ai@users.noreply.github.com>
2022-07-06 23:18:06 +0530

    Update ip_scan.py

--- ip_scan.py
+++ ip_scan.py
@@ -3,5 +3,5 @@ import sys
 from termcolor import colored, cprint
 text = colored('Authore:-Anubhab Mukherjee\n', 'red', attrs=['reverse', 'blink'])
 print(text)
-n=input("Enter your roter ip/cidr(Ex. 192.168.x.x/24): ")
+n=input("Enter your default gateway/cidr(Ex. 192.168.x.x/24): ")
 scapy.arping(n)
