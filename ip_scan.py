515d46b58c076ef61c490510d9faf1c1b65ad792
Anubhab Mukherjee <78078306+Anubhab-ai@users.noreply.github.com>
2022-07-07 00:03:52 +0530

    Update ip_scan.py

--- ip_scan.py
+++ ip_scan.py
@@ -1,8 +1,7 @@
 import scapy.all as scapy
 import sys
 from termcolor import colored, cprint
-text = colored('Authore:-Anubhab Mukherjee\n', 'red', attrs=['reverse', 'blink'])
-print(text)
+cprint('Authore:-Anubhab Mukherjee\n', 'red', attrs=['reverse', 'blink'])
 n=input("Enter your default gateway/cidr(Ex. 192.168.x.x/24): ")
 scapy.arping(n)
 cprint('\n https://github.com/Anubhab-ai\n', 'yellow')
